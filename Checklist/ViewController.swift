4e69d00089365f3d5d6fe29270fff22d9a93594f
Fredrick Osuala <fredrick.osuala@gmail.com>
2023-09-15 21:00:30 +1000

    Initial Commit

---
+++ Checklist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by fredrick osuala on 15/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
